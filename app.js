e4e0bd5cebdc02a579f2567202207b487b1edc85
Valery <119003537+Barnied99@users.noreply.github.com>
2022-11-24 21:14:32 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+
+
+
+window.addEventListener('scroll', e => {
+  document.body.style.cssText += `--scrollTop: ${this.scrollY}px`
+})
+
+
+// gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+// ScrollSmoother.create({
+//   wrapper: '.wrapper',
+//   content: '.content',
+
+// })
+
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+ScrollSmoother.create({
+  wrapper: '.wrapper',
+  content: '.content',
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
